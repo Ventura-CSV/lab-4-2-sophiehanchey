afa308325a4ccf8476f43eb1d6309fef7c54d8ba
Kyu Lee <kwooler@gmail.com>
2023-05-22 22:59:12 -0700

    Lab 4-2

--- main_test.py
+++ main_test.py
@@ -10,18 +10,20 @@ def test_main_1():
     datastr = '5\n2\n3\n1\n5'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
+    assert ret == 16
+
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*16[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*16[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
 
 
 def test_main_2():
@@ -30,15 +32,10 @@ def test_main_2():
     datastr = '1\n2\n3\n4\n5'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*15[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    assert ret == 15
